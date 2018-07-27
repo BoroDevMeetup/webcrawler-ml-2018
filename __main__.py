29ea903914701a93886609052ab0dee5bded8864
Spackman, Neil <Neil.Spackman@asurion.com>
2018-07-27 09:43:41 -0500

    changed prints for settings

--- __main__.py
+++ __main__.py
@@ -6,8 +6,8 @@ parser.add_argument("entry_point", help="Entry point for webcrawler (e.g. https:
 parser.add_argument("--max_pages", help="Maximum pages to crawl, default 10", type=int)
 parser.add_argument("--restrict_domain", help="Restrict navigation to root domain (True or False), default: True", type=bool)
 args = parser.parse_args()
-print("Beginning to crawl...\n")
+print("Beginning to crawl...")
 if args.max_pages: print("Max pages to crawl set to {}".format(args.max_pages))
-if args.restrict_domain: print("Restricting domain")
+print("Restricting domain") if args.restrict_domain else print("Domain not restricted\n...\n")
 crawl = crawler.crawl
 crawl(args.entry_point,args.max_pages,args.restrict_domain)
