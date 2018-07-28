946a8457eb82f47c4791256441793c7f7dd6379c
Spackman, Neil <Neil.Spackman@asurion.com>
2018-07-27 19:47:50 -0500

    added print statements for pages without links, fixed restrict_domain bool and allow more inputs options

--- __main__.py
+++ __main__.py
@@ -1,13 +1,22 @@
 import argparse
 import crawler
 
+def str2bool(v):
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
 parser = argparse.ArgumentParser()
 parser.add_argument("entry_point", help="Entry point for webcrawler (e.g. https://www.google.com)", type=str)
-parser.add_argument("--max_pages", help="Maximum pages to crawl, default 10", type=int)
-parser.add_argument("--restrict_domain", help="Restrict navigation to root domain (True or False), default: True", type=bool)
+parser.add_argument("--max_pages", help="Maximum pages to crawl, default 10", type=int, default=10)
+parser.add_argument("--restrict_domain", help="Restrict navigation to root domain (True or False), default: True", type=str2bool, default=True)
 args = parser.parse_args()
 print("Beginning to crawl...")
 if args.max_pages: print("Max pages to crawl set to {}".format(args.max_pages))
-print("Restricting domain") if args.restrict_domain else print("Domain not restricted\n...\n")
+print("Domain not restricted") if not args.restrict_domain and args.restrict_domain != None else print("Restricting domain")
+print("...\n")
 crawl = crawler.crawl
 crawl(args.entry_point,args.max_pages,args.restrict_domain)
